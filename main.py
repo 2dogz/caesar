d052d5bf7299162b62b57728c8606e60fa4e0e38
Kyle <kylewebb58@yahoo.com>
2016-08-25 22:40:35 -0500

    adding final caesar

--- main.py
+++ main.py
@@ -14,12 +14,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import cgi
+from caesar import encrypt
+from sys import argv, exit
+from helpers import alpha_position, rotate_character
+
+
+class Index(webapp2.RequestHandler):
+    form = """
+    <form method="post">
+        <label>
+            I want to rot13 encrypt
+            <input type="text" name="old_string"/>
+            this text
+    </label>
+    <label>Rotate by: </label>
+    <input type="text" name="rot"/>
+    <input type="submit" value="Encrypt"/>
+    </form>
+    """
     def get(self):
-        self.response.write('Hello world!')
+        #display form
+        self.response.out.write(self.form)
+
+
+    def post(self):
+        #read user input / set as old_string
+        old_string = self.request.get("old_string")
+        old_string = cgi.escape(old_string)
+        rot = self.request.get("rot")
+        rotint = int(rot)
+
+        #write response
+        self.response.out.write("<p>" + old_string + " is the unencrypted version of your word!" + "</p>")
+        new_string = encrypt(old_string, rotint)
+
+        self.response.out.write("<p>" + new_string + " is the encrypted version of your word!" + "</p>")
+
+        #new_string = str(old_string)
+        #new_string = old_string.encrypt(old_string, 13)
+        #self.response.out.write(new_string + " is the encrypted version of your word!")
+
+        #self.response.out.write(old_string)
+        #self.response.out.write(old_string)
+        #self.response.write(new_string + " is the encrypted version of your word!")
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
 ], debug=True)
